13dafd872db442da0fd0bf91aa6e44850fe8ceb9
Arunabh Das <arunabhdas@gmail.com>
2020-07-22 21:08:51 -0400

    Initialized AvocadoToast app

---
+++ avocadotoast/AvocadoToast/AvocadoToast/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  AvocadoToast
+//
+//  Copyright © 2020 Arunabh Das. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Avocado Toast")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
