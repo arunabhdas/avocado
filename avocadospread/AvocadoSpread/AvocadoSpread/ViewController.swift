69d747eb8ffa2ec75f7905b3a737f94c6a1c6ee0
Arunabh Das <arunabhdas@gmail.com>
2020-07-24 10:52:32 -0400

    Added IBOutlets and IBActions

--- avocadospread/AvocadoSpread/AvocadoSpread/ViewController.swift
+++ avocadospread/AvocadoSpread/AvocadoSpread/ViewController.swift
@@ -10,6 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var allowMessagesLabel: UILabel!
+    @IBOutlet weak var allowMessagesSwitch: UISwitch!
+    @IBOutlet weak var sendMesageButton: UIButton!
+    @IBOutlet weak var theMessageLabel: UILabel!
+    
+    @IBAction func sendMessageButtonTapped(_ sender: Any) {
+        
+    }
+    @IBAction func switchToggled(_ sender: Any) {
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
