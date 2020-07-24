81eecd651df01562287017280d2e66af7a01fb0d
Arunabh Das <arunabhdas@gmail.com>
2020-07-24 13:30:32 -0400

    Added setupProcessingChain implementation

--- avocadospread/AvocadoSpread/AvocadoSpread/ViewController.swift
+++ avocadospread/AvocadoSpread/AvocadoSpread/ViewController.swift
@@ -1,12 +1,20 @@
 //
 //  ViewController.swift
 //  AvocadoSpread
-//
-//  Created by Das on 7/24/20.
 //  Copyright © 2020 Arunabh Das. All rights reserved.
 //
 
 import UIKit
+import Combine
+
+extension Notification.Name {
+    static let newMessage = Notification.Name("newMessage")
+}
+
+struct Message {
+    let content: String
+    let author: String
+}
 
 class ViewController: UIViewController {
 
@@ -15,17 +23,38 @@ class ViewController: UIViewController {
     @IBOutlet weak var sendMesageButton: UIButton!
     @IBOutlet weak var theMessageLabel: UILabel!
     
-    @IBAction func sendMessageButtonTapped(_ sender: Any) {
-        
-    }
-    @IBAction func switchToggled(_ sender: Any) {
-        
-    }
+    @Published var canSendMessages:Bool = false
+    
+    private var switchSubscriber: AnyCancellable?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupProcessingChain()
     }
-
+    
+    func setupProcessingChain() {
+        switchSubscriber = $canSendMessages.receive(on: DispatchQueue.main).assign(to: \.isEnabled, on: sendMesageButton)
+        
+        
+        let messagePublisher = NotificationCenter.Publisher(center: .default, name: .newMessage)
+            .map { notification -> String? in
+                return (notification.object as? Message)?.content
+            }
+        
+        let messageSubscriber = Subscribers.Assign(object: theMessageLabel, keyPath: \.text)
+        
+        messagePublisher.subscribe(messageSubscriber)
+    }
+    
+    @IBAction func sendMessageButtonTapped(_ sender: Any) {
+        let message = Message(content: "The, current time is \(Date())", author: "Me")
+        
+        NotificationCenter.default.post(name: .newMessage, object: message)
+    }
+    
+    @IBAction func switchToggled(_ sender: UISwitch) {
+        canSendMessages = sender.isOn
+    }
 
 }
 
